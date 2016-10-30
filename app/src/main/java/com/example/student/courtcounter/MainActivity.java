c90e52dd882bc2a510d9eae3809425678027c049
Chris Garcia <chrisg1245@hotmail.com>
2016-10-29 20:20:54 -0700

    Milestone 2

--- app/src/main/java/com/example/student/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/student/courtcounter/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.student.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+public class MainActivity extends AppCompatActivity{
+    private TextView points;
+
+    public void pointButton(View view) {
+        points.setText("0");
     }
+
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            points = (TextView) findViewById(R.id.textViewName);
+
+        }
 }
