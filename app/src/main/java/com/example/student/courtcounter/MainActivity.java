742646d00ba276749b642a8ecb6c52b672ff418b
Chris Garcia <chrisg1245@hotmail.com>
2016-11-03 18:46:09 -0700

    Revert "milestone 8"
    This reverts commit 7f6bc8ffea88a0689d2ebac7076974a1e827580c.

--- app/src/main/java/com/example/student/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/student/courtcounter/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.student.courtcounter;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,10 +10,11 @@ public class MainActivity extends AppCompatActivity{
     private TextView points;
     private TextView goals;
 
-    private  AlertDialog gameOverAlert;
 
     private double a = 0;
     private double b = 0;
+
+
 
 
     public void pointButton(View view) {
@@ -28,18 +27,7 @@ public class MainActivity extends AppCompatActivity{
         goals.setText(String.valueOf(b));
     }
 
-    public void pointButton3(View view) {
-        a--;
-        points.setText(String.valueOf(a));
-    }
-
-    public void pointButton4(View view) {
-        b--;
-        goals.setText(String.valueOf(b));
-    }
-
-
-    @Override
+        @Override
         protected void onCreate (Bundle savedInstanceState){
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
@@ -50,26 +38,5 @@ public class MainActivity extends AppCompatActivity{
             goals = (TextView) findViewById(R.id.goals);
             goals.setText(String.valueOf(b));
 
-        gameOverAlert= new AlertDialog.Builder(MainActivity.this).create();
-
-        gameOverAlert.setTitle("Alert Title");
-        gameOverAlert.setMessage("Message Text");
-        gameOverAlert.show();
-
-        gameOverAlert.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-
-        if (a > b) {
-            gameOverAlert.setMessage("Team Apple wins");
-        }else if(a < b) {
-            gameOverAlert.setMessage("Team Android wins");
-        }else if (a == b){
-            gameOverAlert.setMessage("Draw");
-        }
-        gameOverAlert.show();
         }
 }
